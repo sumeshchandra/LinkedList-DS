467f4b294a0dffde042b8cd864ea4f21f5cd7e4b
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-07-28 20:47:54 +0530

    Added: LinkedList nodes been Appended

--- src/com/LinkList/MyLinkedList.java
+++ src/com/LinkList/MyLinkedList.java
@@ -23,6 +23,17 @@ public class MyLinkedList {
 		}
 
 	}
+	 public void append(INode myNode){
+	        if (this.head==null){
+	            this.head=myNode;
+	        }
+	        if (this.tail==null){
+	            this.tail=myNode;
+	        }else {
+	            this.tail.setNext(myNode);
+	            this.tail=myNode;
+	        }
+	 }
 
 	public void printNodes() {
 
